306a513b764e756470ecdfe039f4f71cbec5034e
amirmadjour <amirmadjour@kali.KALI>
2024-10-29 00:59:07 +0100

    Graphiques

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -26,10 +26,18 @@ const App = () => {
     index: [],
     data: [],
   });
-  //Not real columns, only the size
-  const [data, setData] = useState([]);
+  const [tabDistance, setTabDistance] = useState({
+    columns: [],
+    index: [],
+    data: [],
+  });
+  const [tabBurt, setTabBurt] = useState({
+    columns: [],
+    index: [],
+    data: [],
+  });
+  const [data, setData] = useState();
   const [error, setError] = useState(null);
-  const [sexe, setSexe] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,22 +48,29 @@ const App = () => {
         const codingTab = JSON.parse(response.data);
         setTabCodage(codingTab);
 
-
         const tabDisjonctifResponse = await axios.get(
           "http://127.0.0.1:8000/coding_table/api/create-coding-table-disjonctif-complet/"
         );
         const disjonctifTab = JSON.parse(tabDisjonctifResponse.data);
         setTabDisjonctif(disjonctifTab);
-        
 
-        const second_res = await axios.get(
-          "http://127.0.0.1:8000/coding_table/api/csv-data/"
+        const tabDistanceResponse = await axios.get(
+          "http://127.0.0.1:8000/coding_table/api/create-distance-table/"
         );
-        const csv_data = JSON.parse(second_res.data);
-        setData(csv_data);
+        const distanceTab = JSON.parse(tabDistanceResponse.data);
+        console.log("DistanceTab: ", distanceTab);
+        setTabDistance(distanceTab);
 
-        const column = csv_data.map((row) => row[0]);
-        setSexe(column);
+        const tabBurtResponse = await axios.get(
+          "http://127.0.0.1:8000/coding_table/api/create-burt-table/"
+        );
+        const BurtTab = JSON.parse(tabBurtResponse.data);
+        setTabBurt(BurtTab);
+
+        const pieResponse = await axios.get(
+          "http://127.0.0.1:8000/coding_table/api/pie-data/"
+        );
+        setData(pieResponse.data);
       } catch (err) {
         setError(err.message);
       }
@@ -64,21 +79,52 @@ const App = () => {
     fetchData();
   }, []);
 
-  const SexePieData = {
-    labels: ["Feminin", "Masculin"],
-    datasets: [
-      {
-        label: "Sexe",
-        data: [15, 85],
-        backgroundColor: ["rgba(255, 99, 132, 0.6", "rgba(54, 162, 235, 0.6)"],
-        hoverOffset: 4,
-      },
-    ],
+  console.log("Pie data:", data);
+
+  const generateGraphics = (data) => {
+    let PieDataArr = [];
+    if (data) {
+      for (const key in data) {
+        console.log("Number: ", key);
+        const PieData = {
+          labels: Object.keys(data[key]),
+          datasets: [
+            {
+              label: "Number",
+              data: Object.values(data[key]),
+              backgroundColor: [
+                "rgba(54, 162, 235, 0.6)",
+                "rgba(255, 99, 132, 0.6)",
+                "rgba(255, 206, 86, 0.6)",
+                "rgba(75, 192, 192, 0.6)",
+                "rgba(153, 102, 255, 0.6)",
+                "rgba(153, 99, 35, 0.6)",
+              ],
+              hoverOffset: 4,
+            },
+          ],
+        };
+        PieDataArr = [...PieDataArr, PieData];
+      }
+    }
+    console.log("PieDataArr: ", PieDataArr);
+    return (
+      <div className="grid grid-cols-3">
+        {data &&
+          PieDataArr &&
+          PieDataArr.map((p) => (
+            <div className="w-full">
+              <Pie data={p} />
+            </div>
+          ))}
+      </div>
+    );
   };
 
   return (
     <div className="flex flex-col items-center justify-center gap-5 py-5 w-2/3">
       {error && <p style={{ color: "red" }}>{error}</p>}
+      {generateGraphics(data)}
       <h2 className="text-gray-700 text-3xl font-bold text-left w-full">
         Tableau de codage
       </h2>
@@ -126,14 +172,52 @@ const App = () => {
         </TableBody>
       </Table>
 
-      <ul>
-        {data.map((i, index) => (
-          <li key={index}>{i}</li>
-        ))}
-      </ul>
-      <div className="w-1/2">
-        <Pie data={SexePieData} />
-      </div>
+      <h2 className="text-gray-700 text-3xl font-bold text-left w-full">
+        Tableau de Distance
+      </h2>
+      <Table className="">
+        <TableHeader>
+          <TableRow>
+            {tabDistance.columns.map((i, index) => (
+              <TableHead key={index}>{i}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {tabDistance.data.map((i, i_index) => (
+            <TableRow key={i_index}>
+              {i.map((j, j_index) => (
+                <TableCell key={i_index.toString() + j_index.toString()}>
+                  {j}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <h2 className="text-gray-700 text-3xl font-bold text-left w-full">
+        Tableau de Burt
+      </h2>
+      <Table className="">
+        <TableHeader>
+          <TableRow>
+            {tabBurt.columns.map((i, index) => (
+              <TableHead key={index}>{i}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {tabBurt.data.map((i, i_index) => (
+            <TableRow key={i_index}>
+              {i.map((j, j_index) => (
+                <TableCell key={i_index.toString() + j_index.toString()}>
+                  {j}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 };
