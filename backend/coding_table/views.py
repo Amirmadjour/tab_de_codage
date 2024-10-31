52f5320411f3920a48815a79b032147151d9efa1
Radhi Badache <radhibadache@gmail.com>
2024-10-31 18:07:16 +0100

    merge branch

--- backend/coding_table/views.py
+++ backend/coding_table/views.py
@@ -17,11 +17,21 @@ def get_csv_data(request):
     try:
         csv_path = os.path.join(os.path.dirname(__file__), 'dataset_forms.csv')
         df = pd.read_csv(csv_path, encoding='utf-8')
-        data = df.to_dict(orient='records')
+        data = df.to_json(orient='values')
         return Response(data)
     except Exception as e:
         return Response({"error": str(e)}, status=500)
 
+@api_view(['GET'])
+def get_pie_data(request):
+    try:
+        csv_path = os.path.join(os.path.dirname(__file__), 'dataset_forms.csv')
+        df = pd.read_csv(csv_path, encoding='utf-8')
+        column_counts = {col: df[col].value_counts().to_dict() for col in df.columns}
+        print(column_counts)
+        return Response(column_counts)
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)
 
 
 @api_view(['GET'])
@@ -32,7 +42,7 @@ def create_coding_table_view(request):
     try:
         df = pd.read_csv(csv_file_path)
         coded_table = create_coding_table(df, ordinal_cols=ordinal_cols)
-        return Response(coded_table.to_json(orient='records'), status=status.HTTP_200_OK)
+        return Response(coded_table.to_json(orient="split"), status=status.HTTP_200_OK)
 
     except FileNotFoundError:
         return Response({"error": "file not found"}, status=status.HTTP_404_NOT_FOUND)
@@ -47,7 +57,7 @@ def create_coding_table_disjonctif_complet_view(request):
     try:
         df = pd.read_csv(csv_file_path)
         coded_table = create_coding_table_disjonctif_complet(df)
-        return Response(coded_table.to_json(orient='records'), status=status.HTTP_200_OK)
+        return Response(coded_table.to_json(orient="split"), status=status.HTTP_200_OK)
 
     except FileNotFoundError:
         return Response({"error": "File not found"}, status=status.HTTP_404_NOT_FOUND)
@@ -63,7 +73,7 @@ def create_tableau_de_distance_view(request):
         df = pd.read_csv(csv_file_path)
         tableau_de_codage = create_coding_table_disjonctif_complet(df)
         tab_de_distance_df = tab_de_distance(tableau_de_codage)
-        return Response(tab_de_distance_df.to_json(orient='records'), status=status.HTTP_200_OK)
+        return Response(tab_de_distance_df.to_json(orient='split'), status=status.HTTP_200_OK)
 
     except FileNotFoundError:
         return Response({"error": "file not found"}, status=status.HTTP_404_NOT_FOUND)
@@ -78,7 +88,7 @@ def create_tableau_de_burt_view(request):
         df = pd.read_csv(csv_file_path)
         tab_de_codage_disjonctif_complet = create_coding_table(df, ordinal_cols='Q3')
         tab_burt_df = tab_burt(tab_de_codage_disjonctif_complet)
-        return Response(tab_burt_df.to_json(orient='records'), status=status.HTTP_200_OK)
+        return Response(tab_burt_df.to_json(orient='split'), status=status.HTTP_200_OK)
 
     except FileNotFoundError:
         return Response({"error": "file not found"}, status=status.HTTP_404_NOT_FOUND)
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,19 +1,225 @@
 "use client";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, Tooltip, Legend, ArcElement } from "chart.js";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
-export default function Home() {
-  const [data, setData] = useState(null);
+ChartJS.register(Tooltip, Legend, ArcElement);
+
+const App = () => {
+  const [tabCodage, setTabCodage] = useState({
+    columns: [],
+    index: [],
+    data: [],
+  });
+  const [tabDisjonctif, setTabDisjonctif] = useState({
+    columns: [],
+    index: [],
+    data: [],
+  });
+  const [tabDistance, setTabDistance] = useState({
+    columns: [],
+    index: [],
+    data: [],
+  });
+  const [tabBurt, setTabBurt] = useState({
+    columns: [],
+    index: [],
+    data: [],
+  });
+  const [data, setData] = useState();
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/coding_table/api/csv-data/")
-      .then((response) => response.json())
-      .then((data) => setData(data));
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://127.0.0.1:8000/coding_table/api/create-coding-table/"
+        );
+        const codingTab = JSON.parse(response.data);
+        setTabCodage(codingTab);
+
+        const tabDisjonctifResponse = await axios.get(
+          "http://127.0.0.1:8000/coding_table/api/create-coding-table-disjonctif-complet/"
+        );
+        const disjonctifTab = JSON.parse(tabDisjonctifResponse.data);
+        setTabDisjonctif(disjonctifTab);
+
+        const tabDistanceResponse = await axios.get(
+          "http://127.0.0.1:8000/coding_table/api/create-distance-table/"
+        );
+        const distanceTab = JSON.parse(tabDistanceResponse.data);
+        console.log("DistanceTab: ", distanceTab);
+        setTabDistance(distanceTab);
+
+        const tabBurtResponse = await axios.get(
+          "http://127.0.0.1:8000/coding_table/api/create-burt-table/"
+        );
+        const BurtTab = JSON.parse(tabBurtResponse.data);
+        setTabBurt(BurtTab);
+
+        const pieResponse = await axios.get(
+          "http://127.0.0.1:8000/coding_table/api/pie-data/"
+        );
+        setData(pieResponse.data);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchData();
   }, []);
 
+  console.log("Pie data:", data);
+
+  const generateGraphics = (data) => {
+    let PieDataArr = [];
+    if (data) {
+      for (const key in data) {
+        console.log("Number: ", key);
+        const PieData = {
+          labels: Object.keys(data[key]),
+          datasets: [
+            {
+              label: "Number",
+              data: Object.values(data[key]),
+              backgroundColor: [
+                "rgba(54, 162, 235, 0.6)",
+                "rgba(255, 99, 132, 0.6)",
+                "rgba(255, 206, 86, 0.6)",
+                "rgba(75, 192, 192, 0.6)",
+                "rgba(153, 102, 255, 0.6)",
+                "rgba(153, 99, 35, 0.6)",
+              ],
+              hoverOffset: 4,
+            },
+          ],
+        };
+        PieDataArr = [...PieDataArr, PieData];
+      }
+    }
+    console.log("PieDataArr: ", PieDataArr);
+    return (
+      <div className="grid grid-cols-3">
+        {data &&
+          PieDataArr &&
+          PieDataArr.map((p) => (
+            <div className="w-full">
+              <Pie data={p} />
+            </div>
+          ))}
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <h1>Data from Django:</h1>
-      {data ? <p>{data.message}</p> : <p>Loading...</p>}
+    <div className="flex flex-col items-center justify-center gap-5 py-5 w-2/3">
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {generateGraphics(data)}
+      <h2 className="text-gray-700 text-3xl font-bold text-left w-full">
+        Tableau de codage
+      </h2>
+      <Table className="">
+        <TableHeader>
+          <TableRow>
+            {tabCodage.columns.map((i, index) => (
+              <TableHead key={index}>V{index}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {tabCodage.data.map((i, i_index) => (
+            <TableRow key={i_index}>
+              {i.map((j, j_index) => (
+                <TableCell key={i_index.toString() + j_index.toString()}>
+                  {j}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <h2 className="text-gray-700 text-3xl font-bold text-left w-full">
+        Tableau de codage disjonctif complet
+      </h2>
+      <Table className="">
+        <TableHeader>
+          <TableRow>
+            {tabDisjonctif.columns.map((i, index) => (
+              <TableHead key={index}>{i}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {tabDisjonctif.data.map((i, i_index) => (
+            <TableRow key={i_index}>
+              {i.map((j, j_index) => (
+                <TableCell key={i_index.toString() + j_index.toString()}>
+                  {j}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <h2 className="text-gray-700 text-3xl font-bold text-left w-full">
+        Tableau de Distance
+      </h2>
+      <Table className="">
+        <TableHeader>
+          <TableRow>
+            {tabDistance.columns.map((i, index) => (
+              <TableHead key={index}>{i}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {tabDistance.data.map((i, i_index) => (
+            <TableRow key={i_index}>
+              {i.map((j, j_index) => (
+                <TableCell key={i_index.toString() + j_index.toString()}>
+                  {j}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <h2 className="text-gray-700 text-3xl font-bold text-left w-full">
+        Tableau de Burt
+      </h2>
+      <Table className="">
+        <TableHeader>
+          <TableRow>
+            {tabBurt.columns.map((i, index) => (
+              <TableHead key={index}>{i}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {tabBurt.data.map((i, i_index) => (
+            <TableRow key={i_index}>
+              {i.map((j, j_index) => (
+                <TableCell key={i_index.toString() + j_index.toString()}>
+                  {j}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
-}
+};
+
+export default App;
