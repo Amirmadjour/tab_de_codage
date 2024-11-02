fdda6e91088542ad651c622334ca205aed873933
Radhi Badache <radhibadache@gmail.com>
2024-11-02 10:59:49 +0100

    implemented contigency tables and upload csv functions

--- backend/coding_table/views.py
+++ backend/coding_table/views.py
@@ -1,97 +1,135 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import pandas as pd
-import os
+import math
 from rest_framework import status
 from .utils import create_coding_table, tab_de_distance
 from .utils import create_coding_table_disjonctif_complet
 from .utils import tab_burt
+from .utils import create_tableau_de_contingence
 
-# test
 @api_view(['GET'])
 def get_data(request):
     data = {"message": "salam from coding table app!"}
     return Response(data)
 
-@api_view(['GET'])
-def get_csv_data(request):
+# fichier csv uploadé
+uploaded_csv_data = {}
+@api_view(['POST'])
+def upload_csv_data(request):
     try:
-        csv_path = os.path.join(os.path.dirname(__file__), 'dataset_forms.csv')
-        df = pd.read_csv(csv_path, encoding='utf-8')
-        data = df.to_json(orient='split')
-        return Response(data)
-    except Exception as e:
-        return Response({"error": str(e)}, status=500)
+        if 'file' not in request.FILES:
+            return Response({"error": "Aucun fichier n'a été envoyé."}, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+        csv_file = request.FILES['file']
+        df = pd.read_csv(csv_file, encoding='utf-8')
+        uploaded_csv_data['df'] = df # stockage dataframe
+
+        return Response({"message": "Fichier CSV uploadé avec succès."}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(['POST'])
 def get_pie_data(request):
     try:
-        csv_path = os.path.join(os.path.dirname(__file__), 'dataset_forms.csv')
-        df = pd.read_csv(csv_path, encoding='utf-8')
+        if 'df' not in uploaded_csv_data:
+            return Response({"error": "Aucun fichier n'a été uploadé. Veuillez uploader le fichier d'abord."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        df = uploaded_csv_data['df']
         column_counts = {col: df[col].value_counts().to_dict() for col in df.columns}
-        print(column_counts)
         return Response(column_counts)
     except Exception as e:
-        return Response({"error": str(e)}, status=500)
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 def create_coding_table_view(request):
-    ordinal_cols = request.query_params.getlist('ordinal_cols', ['Q3']) # notre colonne ordinale
-    csv_file_path = os.path.join(os.path.dirname(__file__), 'dataset_forms.csv')
-
     try:
-        df = pd.read_csv(csv_file_path)
-        coded_table = create_coding_table(df, ordinal_cols=ordinal_cols)
-        return Response(coded_table.to_json(orient="split"), status=status.HTTP_200_OK)
+        if 'df' not in uploaded_csv_data:
+            return Response({"error": "Aucun fichier n'a été uploadé. Veuillez uploader le fichier d'abord."},
+                            status=status.HTTP_400_BAD_REQUEST)
 
-    except FileNotFoundError:
-        return Response({"error": "file not found"}, status=status.HTTP_404_NOT_FOUND)
+        df = uploaded_csv_data['df']
+        ordinal_cols = request.data.get('ordinal_cols', {})
+        if not isinstance(ordinal_cols, dict):
+            return Response({"error": "Le format de 'ordinal_cols' doit être un dictionnaire JSON"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        coded_table = create_coding_table(df, ordinale_order=ordinal_cols)
+        return Response(coded_table.to_json(orient='split'), status=status.HTTP_200_OK)
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-@api_view(['GET'])
+
+@api_view(['POST'])
 def create_coding_table_disjonctif_complet_view(request):
-    csv_file_path = os.path.join(os.path.dirname(__file__), 'dataset_forms.csv')
-
     try:
-        df = pd.read_csv(csv_file_path)
-        coded_table = create_coding_table_disjonctif_complet(df)
-        return Response(coded_table.to_json(orient="split"), status=status.HTTP_200_OK)
+        if 'df' not in uploaded_csv_data:
+            return Response({"error": "Aucun fichier n'a été uploadé. Veuillez uploader le fichier d'abord."},
+                            status=status.HTTP_400_BAD_REQUEST)
 
-    except FileNotFoundError:
-        return Response({"error": "File not found"}, status=status.HTTP_404_NOT_FOUND)
+        df = uploaded_csv_data['df']
+        ordinal_cols = request.data.get('ordinal_cols', {})
+        coded_table = create_coding_table_disjonctif_complet(df, create_coding_table(df, ordinale_order=ordinal_cols))
+        return Response(coded_table.to_json(orient='split'), status=status.HTTP_200_OK)
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 def create_tableau_de_distance_view(request):
-    csv_file_path = os.path.join(os.path.dirname(__file__), 'dataset_forms.csv')
-
     try:
-        df = pd.read_csv(csv_file_path)
-        tableau_de_codage = create_coding_table_disjonctif_complet(df)
-        tab_de_distance_df = tab_de_distance(tableau_de_codage)
-        return Response(tab_de_distance_df.to_json(orient='split'), status=status.HTTP_200_OK)
+        if 'df' not in uploaded_csv_data:
+            return Response({"error": "Aucun fichier n'a été uploadé. Veuillez uploader le fichier d'abord."},
+                            status=status.HTTP_400_BAD_REQUEST)
 
-    except FileNotFoundError:
-        return Response({"error": "file not found"}, status=status.HTTP_404_NOT_FOUND)
+        df = uploaded_csv_data['df']
+        ordinal_cols = request.data.get('ordinal_cols', {})
+        tableau_de_codage = create_coding_table_disjonctif_complet(df, create_coding_table(df, ordinale_order=ordinal_cols))
+        tab_de_distance_df = tab_de_distance(tableau_de_codage)
+
+        return Response(tab_de_distance_df.to_json(orient='split'), status=status.HTTP_200_OK)
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-@api_view(['GET'])
+
+@api_view(['POST'])
 def create_tableau_de_burt_view(request):
-    csv_file_path = os.path.join(os.path.dirname(__file__), 'dataset_forms.csv')
-
     try:
-        df = pd.read_csv(csv_file_path)
-        tab_de_codage_disjonctif_complet = create_coding_table_disjonctif_complet(df)
-        tab_burt_df = tab_burt(tab_de_codage_disjonctif_complet)
-        return Response(tab_burt_df.to_json(orient='split'), status=status.HTTP_200_OK)
+        if 'df' not in uploaded_csv_data:
+            return Response({"error": "Aucun fichier n'a été uploadé. Veuillez uploader le fichier d'abord."},
+                            status=status.HTTP_400_BAD_REQUEST)
 
-    except FileNotFoundError:
-        return Response({"error": "file not found"}, status=status.HTTP_404_NOT_FOUND)
+        df = uploaded_csv_data['df']
+        ordinal_cols = request.data.get('ordinal_cols', {})
+        tab_de_codage_disjonctif_complet = create_coding_table_disjonctif_complet(
+            df, create_coding_table(df, ordinale_order=ordinal_cols))
+        tab_burt_df = tab_burt(tab_de_codage_disjonctif_complet)
+
+        return Response(tab_burt_df.to_json(orient='split'), status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['POST'])
+def create_tableau_de_contigence_view(request):
+    try:
+        if 'df' not in uploaded_csv_data:
+            return Response({"error": "Aucun fichier n'a été uploadé. Veuillez uploader le fichier d'abord."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        df = uploaded_csv_data['df']
+        nombre_tableau_contigence = int(math.factorial(df.shape[1]) / (2 * math.factorial(df.shape[1] - 2)))
+        tab_de_contigence = create_tableau_de_contingence(df)
+
+        tables_json = [table.to_json(orient='split') for table in tab_de_contigence]
+        response_data = {
+            "nombre de tableau de contigence": nombre_tableau_contigence,
+            "tables": tables_json
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
