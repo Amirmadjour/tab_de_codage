aea44cea3a5ebd7425bbaf76db0bec7da7462016
Radhi Badache <radhibadache@gmail.com>
2024-11-10 16:35:36 +0100

    fixed linear regression

--- backend/imputation/utils.py
+++ backend/imputation/utils.py
@@ -2,8 +2,10 @@
 import numpy as np
 import pandas as pd
 
-def simple_linear_regression(data, c1, c2):
+def simple_linear_regression(data):
     # on supprime
+    c1 = data.columns[0]
+    c2 = data.columns[1]
     a = c1
     b = c2
     for j in range(2):
@@ -60,3 +62,13 @@ def multiple_linear_regression(data):
         # on applique la regression multiple pour toutes les colonnes du dataset
         data = MT(data, data.columns[i])
     return data
+
+
+# other linear regressions (dérivés partielles...)
+
+
+# KNN
+
+
+# cosine sina algorithm
+
